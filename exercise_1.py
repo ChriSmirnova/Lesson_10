7a69d2bc4d8e16d4e080973bf8c0eefec83c6ae3
Christina <smirnovaua3@gmail.com>
2024-05-02 14:16:55 +0300

    add exercise_1

---
+++ exercise_1.py
@@ -0,0 +1,19 @@
+def plus(num_1 , num_2):
+    return num_1 + num_2
+result = plus(5 , 5)
+print(result)
+def minus(num_1 , num_2):
+    return num_1 - num_2
+result = minus(10 , 5)
+print(result)
+def multiply(num_1 , num_2):
+    return num_1 * num_2
+result = multiply(10 , 5)
+print(result)
+def divide(num_1 , num_2):
+    return num_1 / num_2
+result = divide(10 , 5)
+print(result)
+
+
+
